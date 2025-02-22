84b569560c1ff2475700d761ba3753f272705d7a
golap-raj <golapraj47@gmail.com>
2025-02-22 18:53:01 +0600

    add js

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll("section");
+for(let section of sections){
+    section.style.border = "2px solid blue";
+    section.style.marginTop = "10px";
+    section.style.borderRadius = "15px";
+    section.style.paddingLeft = "10px";
+    section.style.backgroundColor = "lightgray"
+}
